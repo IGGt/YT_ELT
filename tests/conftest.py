bb19ebc812fdb09c3ebf2a8ecdda862102f130a6
Ian Gates <iang@aeromark.co.uk>
2026-06-05 08:12:36 +0100

    integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,22 @@
 import os
 import pytest
+import psycopg2
 from unittest import mock
 from airflow.models import Variable, Connection, DagBag
 
-@pytest.fixture
+@pytest.fixture()
 def api_key():
     with mock.patch.dict("os.environ",AIRFLOW_VAR_API_KEY="MOCK_KEY1234"):
         yield Variable.get("API_KEY")
 
 
-@pytest.fixture
+@pytest.fixture()
 def channel_handle():
     with mock.patch.dict("os.environ",AIRFLOW_VAR_CHANNEL_HANDLE="MRCHEESE"):
         yield Variable.get("CHANNEL_HANDLE")
 
 
-@pytest.fixture
+@pytest.fixture()
 def mock_postgres_conn_vars():
     conn = Connection(
         login="mock_username",
@@ -30,6 +31,44 @@ def mock_postgres_conn_vars():
         yield Connection.get_connection_from_secrets(conn_id="POSTGRES_DB_YT_ELT")
 
 
-@pytest.fixture
+@pytest.fixture()
 def dagbag():
     yield DagBag()
+
+
+@pytest.fixture()
+def airflow_variable():
+    def get_airflow_variable(variable_name):
+        env_var = f"AIRFLOW_VAR_{variable_name.upper()}"
+        return os.getenv(env_var)
+
+    return get_airflow_variable
+
+
+@pytest.fixture()
+def real_postgres_connection():
+    dbname = os.getenv("ELT_DATABASE_NAME")
+    user = os.getenv("ELT_DATABASE_USERNAME")
+    password = os.getenv("ELT_DATABASE_PASSWORD")
+    host = os.getenv("ELT_DATABASE_HOST")
+    port = os.getenv("ELT_DATABASE_PORT")
+
+    conn = None
+
+    try:
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+
+        yield conn
+    
+    except psycopg2.Error as e:
+        pytest.fail(f"Failed to connect to the database: {e}")
+
+    finally:
+        if conn:
+            conn.close()
